d09ed94b097f415c68032efefd8f9a5703905f0e
JUn1c0rn <llly1991@hotmail.com>
2020-07-13 11:04:19 +0800

    add vue and fastify

--- backend/fastify/test/services/example/index.js
+++ backend/fastify/test/services/example/index.js
@@ -1,17 +1,17 @@
 'use strict'
 
-module.exports = function (fastify, opts, next) {
-  fastify.get('/example', function (request, reply) {
-    reply.send('this is an example')
-  })
+// module.exports = function (fastify, opts, next) {
+//   fastify.get('/example', function (request, reply) {
+//     reply.send('this is an example')
+//   })
 
-  next()
-}
+//   next()
+// }
 
 // If you prefer async/await, use the following
 //
-// module.exports = async function (fastify, opts) {
-//   fastify.get('/example', async function (request, reply) {
-//     return 'this is an example'
-//   })
-// }
+module.exports = async function (fastify, opts) {
+  fastify.get('/example', async function (request, reply) {
+    return 'this is an example'
+  })
+}
